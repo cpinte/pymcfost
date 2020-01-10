9bd2371cae5ee93b5b026d3cc81f22ed85f09581
Clément Robert <cr52@protonmail.com>
2020-01-10 09:42:59 +0100

    more inspection in the test

--- tests/test_io_params.py
+++ tests/test_io_params.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from copy import copy
 
 import pytest
 from pymcfost import Params
@@ -16,8 +17,14 @@ def test_io_copy(parafile):
     """Read a valid .para file, check that it can still be read and written with no data corruption"""
     os.makedirs(output_dir, exist_ok=True)
     p1 = Params(str(parafile))
+    s1 = copy(str(p1))
+
     output_file = str(output_dir / "_".join(["copy", parafile.name]))
     p1.writeto(output_file)
     p2 = Params(output_file)
+    s2 = str(p2)
 
-    assert str(p1) == str(p2)
+    s3 = str(p1)
+    assert s3 == s1
+    assert s2 == s1
+
