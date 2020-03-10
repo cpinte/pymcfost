b3ad35aee93c73cb3b89902ae386e241c574aeac
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2020-03-10 16:49:01 +1100

    Bug fix: ax not stored properly

--- pymcfost/line.py
+++ pymcfost/line.py
@@ -20,14 +20,14 @@ from .utils import FWHM_to_sigma, default_cmap, Wm2_to_Tb, Jy_to_Tb
 
 
 class Map:
-    
+
     # see Line plot_map for details on each parameter
     def __init__(self,
         i, iaz, iTrans, v, iv, insert, substract_cont, moment,
         psf_FWHM, bpa, plot_beam, axes_unit, conv_method,
         colorbar, cmap, ax, no_xlabel, no_ylabel, no_xticks,
         no_yticks, title, limit, limits, Delta_v, shift_dx, shift_dy, plot_stars, bmin, bmaj, line=None):
-        
+
         self.i = i
         self.iaz = iaz
         self.iTrans = iTrans
@@ -54,16 +54,16 @@ class Map:
         self.Delta_v = Delta_v
         self.shift_dx = shift_dx
         self.shift_dy = shift_dy
-        self.plot_stars = plot_stars 
-               
+        self.plot_stars = plot_stars
+
         self.im = None
         self.image = None
         self.moments = None
-        
+
         # instance of Line class
         self.line = line
         self.norm = None
-        
+
         self.bmin = bmin
         self.bmaj = bmaj
 
@@ -74,7 +74,7 @@ class Map:
             return self.line.get_moment_map(self, moment)
         else:
             return self.moments[moment]
-    
+
     def create_colorbar(self, label):
         if self.colorbar:
             divider = make_axes_locatable(self.ax)
@@ -94,8 +94,8 @@ class Map:
             return "Velocity dispersion [km.s$^{-1}$]"
         else:
             return "Flux [" + formatted_unit + "]"
-    
-            
+
+
     def add_beam_stars_vlabels(self, ax, bmin, bmaj, line):
         # -- Adding velocity
         have_label = False
@@ -128,14 +128,14 @@ class Map:
             self.show_stars(ax)
 
     def format_plot(self, ax, no_xlabel=None, no_ylabel=None, title=None):
-        
+
         if no_xlabel is None:
             no_xlabel = self.no_xlabel
         if no_ylabel is None:
             no_ylabel = self.no_ylabel
         if title is None:
             title = self.title
-    
+
         if self.limit is not None:
             self.limits = [-self.limit, self.limit, -self.limit, self.limit]
 
@@ -158,7 +158,7 @@ class Map:
 
     def set_ax(self, ax):
         self.ax = ax
-            
+
     def iv_labels_extent(self, line, axes_unit):
 
         # -- Selecting channel corresponding to a given velocity
@@ -184,8 +184,8 @@ class Map:
             raise ValueError("Unknown unit for axes_units: " + axes_unit)
         halfsize = np.asarray(line.lines.shape[-2:]) / 2 * pix_scale
         self.extent = [halfsize[0] - self.shift_dx, -halfsize[0] - self.shift_dx, -halfsize[1] - self.shift_dy, halfsize[1] - self.shift_dy]
-        
-    
+
+
     def i_convolving(self, bmin, bmaj, line):
         # -- beam or psf : psf_FWHM and bmaj and bmin are in arcsec, bpa in deg
         i_convolve = False
@@ -214,7 +214,7 @@ class Map:
             self.conv_method = convolve_fft
 
         return i_convolve
-            
+
     def create_channel_im(self, i_convolve, Tb, line):
         # individual channel
         if line.is_casa:
@@ -250,7 +250,7 @@ class Map:
                     im[i, j] = np.average(f(v_new))
         else:
             im = cube[self.iv, :, :]
-            
+
         # -- Convolve image
         if i_convolve:
             im = self.conv_method(im, self.beam)
@@ -267,7 +267,7 @@ class Map:
 #             print("Max Tb=", np.max(im), "K")
 
         self.im = im
-            
+
     def create_color_scale(self, fmin, fmax, fpeak, color_scale, dynamic_range):
 
         if fmax is None:
@@ -291,31 +291,31 @@ class Map:
             self.norm = colors.PowerNorm(gamma=1./2., vmin=fmin, vmax=fmax, clip=True)
         else:
             raise ValueError("Unknown color scale: " + color_scale)
-            
-            
+
+
     def show_stars(self, ax, which="all"):
         # add being able to choose colour and style
-        
+
         if which not in ["primary", "companion", "binary", "all"]:
             raise ValueError("invalid stars requested. Must be one of primary, secondary, or all")
-    
+
         if self.moment is None or self.moment != 1:
                 color = "cyan"
         else:
             color = "black"
-            
+
         x_star = self.line.star_positions[0, self.iaz, self.i, :]
-        y_stars = self.line.star_positions[1, self.iaz, self.i, :]      
+        y_stars = self.line.star_positions[1, self.iaz, self.i, :]
 
         if len(x_star) == 2:
             which = "binary"
         elif which == "all":
-            ax.scatter(x_star, y_stars, color=color, s=10, edgecolors='black', linewidth=0.2, marker='o')  
-            return 
+            ax.scatter(x_star, y_stars, color=color, s=10, edgecolors='black', linewidth=0.2, marker='o')
+            return
 
         if which in ["primary", "binary"]:
             ax.scatter(x_star[0], y_stars[0], color=color, s=20, edgecolors='black', linewidth=0.2, marker='*')
-            
+
         if which in ["companion", "binary"]:
             ax.scatter(x_star[1], y_stars[1], color=color, s=10, edgecolors='black', linewidth=0.2, marker='o')
 
@@ -400,7 +400,7 @@ class Line:
     def plot_map(
         self,
         i=0,                        # (int) inclination index
-        iaz=0,                      # (int) azimuth index 
+        iaz=0,                      # (int) azimuth index
         iTrans=0,                   # ?
         v=None,                     # (float) Velocity channel plotted (km/s)
         iv=None,                    # ?
@@ -435,21 +435,21 @@ class Line:
         shift_dy=0,                 # ?
         plot_stars=False,           # (bool) plot all stars, or no stars
         subtractor=None             # (Map) pre-calculated map, which is used to take away from this instance
-                                    #       e.g. if we wanted to create a M1 residual plot 
+                                    #       e.g. if we wanted to create a M1 residual plot
     ):
-        
+
         # Puts properties into a wrapper class for later use
         map = Map(i, iaz, iTrans, v, iv, insert, substract_cont, moment,
         psf_FWHM, bpa, plot_beam, axes_unit, conv_method,
         colorbar, cmap, ax, no_xlabel, no_ylabel, no_xticks,
         no_yticks, title, limit, limits, Delta_v, shift_dx, shift_dy, plot_stars, bmin, bmaj, line=self)
-            
+
         self.subtractor = subtractor
         map.Tb = Tb
 
         if ax is None:
             ax = plt.gca()
-        
+
         map.iv_labels_extent(self, axes_unit)
         # -- set color map
         if cmap is None:
@@ -457,23 +457,23 @@ class Line:
                 cmap = "RdBu_r"
             else:
                 cmap = default_cmap
-                
+
         map.cmap = cmap
 
         i_convolve = map.i_convolving(bmin, bmaj, self)
-        
+
 
         # -- Selection of image to plot
-        if moment is not None:            
+        if moment is not None:
             map.im = self.get_moment_map(map, moment)
         else:
             map.create_channel_im(i_convolve, Tb, self)
-        
+
         if subtractor is not None:
             if subtractor.moment != moment:
                 raise ValueError("Subtractor map does not have the correct moment")
             map.im -= subtractor.im
-                    
+
         im = map.im
         if Tb:
             if self.is_casa:
@@ -483,7 +483,9 @@ class Line:
                 im = np.nan_to_num(im)
             print("Max Tb=", np.max(im), "K")
             print("Min Tb=", np.min(im), "K")
+
         map.im = im
+        map.ax = ax
 
         map.create_color_scale(fmin, fmax, fpeak, color_scale, dynamic_range)
         map.create_plot(map.ax, cmap)
@@ -492,7 +494,7 @@ class Line:
 
         if colorbar:
             map.create_colorbar(map.create_cb_label())
-        
+
         return map
 
     def plot_line(
@@ -553,8 +555,8 @@ class Line:
         except:
             pass
         return M0
-    
-    
+
+
     def calc_M0(self, moments, beam, cube, conv_method, original_moment):
         """ Calcualtes moment 0 """
         if beam is None:
@@ -562,30 +564,30 @@ class Line:
         else:
             if original_moment == 0:
                 return conv_method(np.sum(cube, axis=0) * self.dv, beam)
-            else:                   
+            else:
                 return self.convolve_channels(cube, beam, conv_method)
-    
+
     def calc_moment(self, moment, beam, cube, conv_method, moments, original_moment):
         """ Calculates and return an indiviudal moment. Assumes moments needed for each one have already been calculated"""
-    
+
         if moment == 0:
             return self.calc_M0(moments, beam, cube, conv_method, original_moment)
-            
+
         elif moment == 1:
             return np.sum(cube[:, :, :] * self.velocity[:, np.newaxis, np.newaxis], axis=0) * self.dv / moments[0]
-            
+
         elif moment == 2:
             return np.sqrt(np.sum(cube[:, :, :]
                     * (self.velocity[:, np.newaxis, np.newaxis] - moments[1][np.newaxis, :, :]) ** 2,
                     axis=0,) * self.dv / moments[0])
-        
-        
-    
+
+
+
     def calc_moments(self, moment, beam, cube, conv_method, moments):
         """ Calculates a set of mathematical moments and assigns them to the appropriate index of the moments list"""
         if moment == 9:
                 return self.velocity[(np.argmax(cube, axis=0))]
-                
+
         for i in range(moment + 1):
             moments[i] = self.calc_moment(i, beam, cube, conv_method, moments, moment)
 
@@ -593,15 +595,15 @@ class Line:
         """
         Calculates and returns a moment for a given map, with a predetermined set of properties.
         Moments calculated are saved within the map for later use.
-    
+
         Parameters:
             map (Map): Container for the properties needed to calculate the moment map
             moment (int): moment which will be calculated
-    
+
         Returns : The moment maps in physical units, ie:
                 - M1 is the average velocity [km/s]
                 - M2 is the velocity dispersion [km/s]
-         
+
         """
         i = map.i
         iaz = map.iaz
@@ -609,29 +611,29 @@ class Line:
         beam = map.beam
         conv_method = map.conv_method
         substract_cont = map.substract_cont
-        
+
         # currently does not account for moments greater than M2
         if map.moments is None:
             map.moments = [None] * 3
 
         self.dv = self.velocity[1] - self.velocity[0]
-        
+
         if self.is_casa:
             cube = np.copy(self.lines[:, :, :])
         else:
             cube = np.copy(self.lines[iaz, i, iTrans, :, :, :])
         if substract_cont:
             cube = np.maximum(cube - self.cont[iaz, i, iTrans, np.newaxis, :, :], 0.0)
-        
+
         self.calc_moments(moment, beam, cube, conv_method, map.moments)
 
         return map.moments[moment]
-                   
-                   
-def plot_contours(map, moment, levels=4, ax=None, specific_values=[], colors='black', linewidths=0.25):    
+
+
+def plot_contours(map, moment, levels=4, ax=None, specific_values=[], colors='black', linewidths=0.25):
     """
     Overplot contours of a given map and moment onto an axis.
-    
+
     Parameters:
         map (Map): map of data to be plotted
         moment (int): The moment that will be the source of the contour lines
@@ -641,7 +643,7 @@ def plot_contours(map, moment, levels=4, ax=None, specific_values=[], colors='bl
         specific_values (List<int>): Specific contour levels that can be specified to be plotted. Must be in ascending order
         colors: (str or List<str>): Colors for the contour lines/levels
         linewidths= (int or List<int>): Line thickness of contour lines/levels
-        
+
     Example:
         If we wanted to plot v=0, v=-1.5 and v=+1.5 of a map onto an axis...
         plot_contours(map, 1, specific_values=[-1.5, 0, 1.5])
@@ -649,19 +651,19 @@ def plot_contours(map, moment, levels=4, ax=None, specific_values=[], colors='bl
     im = map.get_moment(moment)
     if ax is None:
         ax = map.ax
-    
+
     if len(specific_values) != 0:
         levels = specific_values
-    
+
     ax.contour(im, extent=map.extent, origin='lower', levels=levels, colors=colors, linewidths=linewidths)
-    
+
 
 def replot(ax, map, no_xlabel=None, no_ylabel=None, title=None, cmap=None):
-    """ 
+    """
     Allows for an already processed map to be plotted on a different set of axis.
     This may be done to look at how a plot may look with different cmaps, or to overplot
     contours or stars.
-    
+
     Parameters:
         ax (plt.axis): axis the map is being plotted onto
         map (pym.Map): previously processed Map instance (e.g. a map that has already been
@@ -670,32 +672,31 @@ def replot(ax, map, no_xlabel=None, no_ylabel=None, title=None, cmap=None):
     """
     if cmap is None:
         cmap = map.cmap
-    
+
     map.create_plot(ax, cmap)
     map.format_plot(ax, no_xlabel, no_ylabel, title)
     map.add_beam_stars_vlabels(ax, map.bmin, map.bmaj, map.line)
-    
-    
+
+
 def create_colorbar(figure, map, ax, fontsize='18', rotation=270, tick_label_size=16):
     """
     Places a colorbar with the fmin and fmax, and the appropriate label sourced from the given map.
     Ensure fmin/fmax are the same or have been scaled appropriately if using one colorbar for many plots
-    
+
     The below will produce a colorbar along the side of a group of subplots, with the fmin and fmax values
     specified in the m1_map, as well as the appropriate label from it.
-    
+
         fig, allAxes = plt.subplots(....)
         ...
         m1_map = data.plot_map(...ax=allAxes[j][i]...)
         ...
         create_colorbar(fig, m1_map, allAxes)
-        
+
     Returns a colorbar instance
     """
     cb = figure.colorbar(map.image, ax=ax, orientation='vertical')
     label = map.create_cb_label()
     cb.set_label(label, fontsize=fontsize, rotation=rotation, labelpad=30)
-    cb.ax.tick_params(labelsize=tick_label_size) 
-    
+    cb.ax.tick_params(labelsize=tick_label_size)
+
     return cb
-    
