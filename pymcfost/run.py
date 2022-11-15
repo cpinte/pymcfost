3ef4ca85b7db234e8683b828390bb57d7f25f50e
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2022-11-15 14:29:28 +1100

    Adding option for log and root_dir in run

--- pymcfost/run.py
+++ pymcfost/run.py
@@ -3,7 +3,7 @@ import subprocess
 
 _mcfost_bin = "mcfost"
 
-def run(filename, options="", delete_previous=False, notebook=False):
+def run(filename, options="", delete_previous=False, notebook=False, logfile=None):
 
     if not isinstance(filename, str):
         raise TypeError("First argument to run must be a filename.")
@@ -11,8 +11,20 @@ def run(filename, options="", delete_previous=False, notebook=False):
     if not os.path.exists(filename):
         raise IOError(filename+" does not exist")
 
+    # Finding root directory
+    root_dir = "."
+    opts = options.split()
+    for i, option in enumerate(opts):
+        if option == "-root_dir":
+            root_dir = opts[i+1]
+            break
+
     if delete_previous:
-        subprocess.call("rm -rf data_* ", shell = True)
+        subprocess.call("rm -rf "+root_dir+"/data_*", shell = True)
+
+    # Saving output in a log file
+    if logfile is not None:
+        options+= " > "+logfile
 
     print("pymcfost: Running mcfost ...")
     r = subprocess.run(_mcfost_bin+" "+filename+" "+options, shell = True)
