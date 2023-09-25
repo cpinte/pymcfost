3f8fd40ad915b83dbd3955bb1d73e14d3fc41cb9
王梓先 <wang.zixian.astro@gmail.com>
2023-09-25 21:20:31 +1000

    build test

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,24 @@
-import os
+# import os
+#
+# working_dir = os.getcwd()
 
-working_dir = os.getcwd()
-print(working_dir)
+
+from pathlib import Path
+import os
+from copy import copy
+
+import pytest
+from pymcfost import Params
+
+test_dir = Path(__file__).parent
+
+output_dir = test_dir / "artifacts"
+input_dir = test_dir / "corpus"
+corpus = list(input_dir.glob("*.para"))
+
+print(output_dir)
+print(input_dir)
+print(corpus)
+
+
+
