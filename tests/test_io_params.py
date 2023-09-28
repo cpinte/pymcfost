832b15232098faccb1e1cbae23ada7c275379fb9
王梓先 <wang.zixian.astro@gmail.com>
2023-09-28 15:16:22 +1000

    Tidy up some codes and change the data to tar.gz

--- tests/test_io_params.py
+++ tests/test_io_params.py
@@ -6,17 +6,14 @@ import pytest
 from pymcfost import Params
 
 
-os.system('wget http://www.physics.usyd.edu.au/~zwan0382/pymcfost/testdata.tar')
-os.system('tar -xvf testdata.tar -C ./tests/')
-
 test_dir = Path(__file__).parent
 
 output_dir = test_dir / "artifacts"
 input_dir = test_dir / "corpus"
 
 if os.path.exists(str(input_dir)) == False:
-    os.system('wget http://www.physics.usyd.edu.au/~zwan0382/pymcfost/testdata.tar')
-    os.system('tar -xvf testdata.tar -C ./tests/')
+    os.system('wget http://www.physics.usyd.edu.au/~zwan0382/pymcfost/testdata.tar.gz')
+    os.system('tar -xvf testdata.tar.gz -C ./tests/')
 
 corpus = list(input_dir.glob("*.para"))
 
