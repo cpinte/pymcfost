935d7f16e66dc1fc241e43bbc36a761dd823a673
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2025-02-28 12:11:56 +1100

    Doc: updating path

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath(
+    os.path.join(__file__, "../../src")
+))
+
 
 # -- Project information -----------------------------------------------------
 project = 'pymcfost'
