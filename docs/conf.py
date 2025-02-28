871a3a7385d71653902bb28cc145532115683485
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2025-02-28 12:45:57 +1100

    Doc: updating path

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../src")
-))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/pymcfost")))
 
 
 # -- Project information -----------------------------------------------------
